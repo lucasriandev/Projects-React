83a45a2d3924a161daf30dce1f15c31010a41cc0
Lucas <lucasrian.dev@gmail.com>
2026-02-24 18:16:21 -0300

    testando aprendizado

--- meu-primeiro-projeto-react/src/Profile.jsx
+++ meu-primeiro-projeto-react/src/Profile.jsx
@@ -3,10 +3,15 @@ import { useState } from "react";
 function Profile(props) {
   //criando o estado curtidas começando com 0
   const [curtidas, setCurtidas] = useState(0);
+  const [favorito, setFavorito] = useState(0);
 
   //sera chamada quando o botao clicar
   function adicionarCurtidas() {
     setCurtidas(curtidas + 1); //atualiza o estado somando 1
+  }
+
+  function adicionarFavorito() {
+    setFavorito(favorito + 1);
   }
 
   return (
@@ -15,8 +20,10 @@ function Profile(props) {
       <p>{props.cargo}</p>
 
       <p>❤️ Curtidas: {curtidas}</p>
+      <p>⭐ favorito: {favorito}</p>
 
       <button onClick={adicionarCurtidas}>Curtir perfil</button>
+      <button onClick={adicionarFavorito}>Colocar nos favoritos</button>
     </div>
   );
 }
