874ff6a0333390fc9eca7e5051a6a390495ee5fe
Lucas <lucasrian.dev@gmail.com>
2026-02-24 18:06:26 -0300

    aprendendo useState

--- meu-primeiro-projeto-react/src/Profile.jsx
+++ meu-primeiro-projeto-react/src/Profile.jsx
@@ -1,8 +1,22 @@
-function Profile(parametro) {
+import { useState } from "react";
+
+function Profile(props) {
+  //criando o estado curtidas começando com 0
+  const [curtidas, setCurtidas] = useState(0);
+
+  //sera chamada quando o botao clicar
+  function adicionarCurtidas() {
+    setCurtidas(curtidas + 1); //atualiza o estado somando 1
+  }
+
   return (
     <div className="profile-card">
-      <h2>{parametro.nome}</h2>
-      <p>{parametro.cargo}</p>
+      <h2>{props.nome}</h2>
+      <p>{props.cargo}</p>
+
+      <p>❤️ Curtidas: {curtidas}</p>
+
+      <button onClick={adicionarCurtidas}>Curtir perfil</button>
     </div>
   );
 }
