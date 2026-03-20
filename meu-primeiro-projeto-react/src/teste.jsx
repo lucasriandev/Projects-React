242579c7b2c62df0d56cb2fad25b1fa623d1a908
Lucas <lucasrian.dev@gmail.com>
2026-03-20 19:45:49 -0300

    quase dominando de eventos

--- meu-primeiro-projeto-react/src/teste.jsx
+++ meu-primeiro-projeto-react/src/teste.jsx
@@ -1,67 +1,48 @@
 import { useState, useEffect } from "react";
 
-function Trabalhos() {
-  const [trabalhos, setTrabalhos] = useState(() => {
-    return JSON.parse(localStorage.getItem("Trabalhos")) || [];
+function Blinders() {
+  const [personagem, setPersonagem] = useState(() => {
+    return JSON.parse(localStorage.getItem("gangue")) || [];
   });
 
-  const [trabalhosNovos, setTrabalhosNovos] = useState("");
+  const [NovoPersonagem, setNovoPersonagem] = useState("");
 
   useEffect(() => {
-    localStorage.setItem("Trabalhos", JSON.stringify(trabalhos));
-  }, [trabalhos]);
+    localStorage.setItem("gangue", JSON.stringify(personagem));
+  }, [personagem]);
 
   function input(event) {
-    setTrabalhosNovos(event.target.value);
+    setNovoPersonagem(event.target.value);
   }
 
   function add() {
-    if (trabalhosNovos !== "") {
-      setTrabalhos([...trabalhos, { nome: trabalhosNovos, feito: false }]);
-      setTrabalhosNovos("");
+    if (NovoPersonagem !== "") {
+      setPersonagem([...personagem, NovoPersonagem]);
+      setNovoPersonagem("");
     }
   }
 
-  function alternarFeitos(indexClicado) {
-    const novaLista = trabalhos.map((item, index) => {
-      if (index === indexClicado) {
-        return {
-          ...item,
-          feito: !item.feito,
-        };
-      }
-      return item;
-    });
-    setTrabalhos(novaLista);
-  }
-
-  function remover(indexRemove) {
-    const listaNova = trabalhos.filter((item, index) => index !== indexRemove);
-    setTrabalhos(listaNova);
+  function remove(indexRemover) {
+    const listaNova = personagem.filter(
+      (perso, index) => index !== indexRemover,
+    );
+    setPersonagem(listaNova);
     if (listaNova.length === 0) {
-      localStorage.removeItem("Trabalhos");
+      localStorage.removeItem("gangue");
     }
   }
 
   return (
     <div>
-      <h1>Trabalhos de aulas!</h1>
-      <input
-        type="text"
-        value={trabalhosNovos}
-        onChange={input}
-        placeholder="Digite seu trabalho!"
-      />
-      <button onClick={add}>Adicionar: </button>
+      <h1>Quem vai morrer?</h1>
+      <input type="text" value={NovoPersonagem} onChange={input} />
+      <button onClick={add}>Adicionar</button>
 
       <ul>
-        {trabalhos.map((trabalhos, index) => (
+        {personagem.map((item, index) => (
           <li key={index}>
-            {trabalhos.nome}
-            <button onClick={() => remover(index)}>Apagar</button>
-            <button onClick={() => alternarFeitos(index)}>
-              {trabalhos.feito ? "TRABALHO FEITO" : "PENDENTE"}
-            </button>
+            {item}
+            <button onClick={() => remove(index)}>remover</button>
           </li>
         ))}
       </ul>
@@ -69,4 +50,4 @@ function Trabalhos() {
   );
 }
 
-export default Trabalhos;
+export default Blinders;
