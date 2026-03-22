1746d11cd5bb2b10b2b1b7346d05b4b0ae385be3
Lucas <lucasrian.dev@gmail.com>
2026-03-22 18:18:48 -0300

    terminar amanha

--- meu-primeiro-projeto-react/src/teste.jsx
+++ meu-primeiro-projeto-react/src/teste.jsx
@@ -1,53 +1,5 @@
 import { useState, useEffect } from "react";
 
-function Blinders() {
-  const [personagem, setPersonagem] = useState(() => {
-    return JSON.parse(localStorage.getItem("gangue")) || [];
-  });
+function Final() {}
 
-  const [NovoPersonagem, setNovoPersonagem] = useState("");
-
-  useEffect(() => {
-    localStorage.setItem("gangue", JSON.stringify(personagem));
-  }, [personagem]);
-
-  function input(event) {
-    setNovoPersonagem(event.target.value);
-  }
-
-  function add() {
-    if (NovoPersonagem !== "") {
-      setPersonagem([...personagem, NovoPersonagem]);
-      setNovoPersonagem("");
-    }
-  }
-
-  function remove(indexRemover) {
-    const listaNova = personagem.filter(
-      (perso, index) => index !== indexRemover,
-    );
-    setPersonagem(listaNova);
-    if (listaNova.length === 0) {
-      localStorage.removeItem("gangue");
-    }
-  }
-
-  return (
-    <div>
-      <h1>Quem vai morrer?</h1>
-      <input type="text" value={NovoPersonagem} onChange={input} />
-      <button onClick={add}>Adicionar</button>
-
-      <ul>
-        {personagem.map((item, index) => (
-          <li key={index}>
-            {item}
-            <button onClick={() => remove(index)}>remover</button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
-
-export default Blinders;
+export default Final;
