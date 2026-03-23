e84d73bbcbe2bf4b8be0c22e6b9c2b75776d6ef8
Lucas <lucasrian.dev@gmail.com>
2026-03-23 17:00:25 -0300

    localstorage dominado

--- meu-primeiro-projeto-react/src/teste.jsx
+++ meu-primeiro-projeto-react/src/teste.jsx
@@ -1,5 +1,58 @@
 import { useState, useEffect } from "react";
 
-function Final() {}
+function Personagens() {
+  const [personagem, setPersonagem] = useState(() => {
+    return JSON.parse(localStorage.getItem("Favorito")) || [];
+  });
 
-export default Final;
+  const [novoPersonagem, setNovoPersonagem] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("Favorito", JSON.stringify(personagem));
+  }, [personagem]);
+
+  function input(event) {
+    setNovoPersonagem(event.target.value);
+  }
+
+  function add() {
+    if (novoPersonagem !== "") {
+      setPersonagem([...personagem, novoPersonagem]);
+      setNovoPersonagem("");
+    }
+  }
+
+  function remover(indexRemove) {
+    const novaLista = personagem.filter(
+      (person, index) => index !== indexRemove,
+    );
+    setPersonagem(novaLista);
+    if (novaLista.length === 0) {
+      localStorage.removeItem("Favorito");
+    }
+  }
+
+  return (
+    <div>
+      <h1>Personagens favoritos</h1>
+      <input
+        type="text"
+        value={novoPersonagem}
+        onChange={input}
+        placeholder="Digite"
+      />
+      <button onClick={add}>Adicionar</button>
+
+      <ul>
+        {personagem.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button onClick={() => remover(index)}>REMOVER</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Personagens;
