138e057c47851f2d8b7e2ede032b9ade6a036702
Lucas <lucasrian.dev@gmail.com>
2026-03-28 14:39:27 -0300

    treinando crud

--- meu-primeiro-projeto-react/src/Sobre.jsx
+++ meu-primeiro-projeto-react/src/Sobre.jsx
@@ -1,8 +1,50 @@
+import { useEffect, useState } from "react";
+
 function Sobre() {
+  const [usuario, setUsuario] = useState(() => {
+    return JSON.parse(localStorage.getItem("User")) || [];
+  });
+
+  const [novoUsuario, setNovoUsuario] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("User", JSON.stringify(usuario));
+  }, [usuario]);
+
+  function add() {
+    if (novoUsuario !== "") {
+      setUsuario([...usuario, novoUsuario]);
+      setNovoUsuario("");
+    }
+  }
+
+  function remove(indexRemover) {
+    const listaNova = usuario.filter((t, index) => index !== indexRemover);
+    setUsuario(listaNova);
+    if (listaNova.length === 0) {
+      localStorage.removeItem("User");
+    }
+  }
+
   return (
     <div>
-      <h2>📖 Sobre Nós</h2>
-      <p>Este é um projeto de estudo do Dia 7.</p>
+      <h1>Cadastro de usuario!</h1>
+      <input
+        type="text"
+        value={novoUsuario}
+        placeholder="Digite!"
+        onChange={(e) => setNovoUsuario(e.target.value)}
+      />
+      <button onClick={add}>Adicionar</button>
+
+      <ul>
+        {usuario.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button onClick={() => remove(index)}>Remover</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
--- meu-primeiro-projeto-react/src/teste.jsx
+++ meu-primeiro-projeto-react/src/teste.jsx
@@ -1,41 +1,5 @@
 import { useState, useEffect } from "react";
 
-function Api() {
-  const [usuario, setUsuario] = useState([]);
-  const [input, setInput] = useState("");
-  const [busca, setBusca] = useState("");
-
-  useEffect(() => {
-    if (busca === "") return;
-    fetch(`https://api.tvmaze.com/search/shows?q=${busca}`)
-      .then((res) => res.json())
-      .then((dados) => {
-        console.log(dados);
-        setUsuario(dados);
-      })
-      .catch((Error) => {
-        console.log(Error, "errooooo");
-      });
-  }, [busca]);
-
-  return (
-    <div>
-      <h1>Heroi</h1>
-      <input
-        type="text"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        placeholder="Digite"
-      />
-
-      <button onClick={() => setBusca(input)}>Buscar</button>
-      <ul>
-        {usuario.map((item, index) => (
-          <li key={index}>{item.show.name}</li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+function Api() {}
 
 export default Api;
