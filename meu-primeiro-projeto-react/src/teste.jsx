8c015ca1d2fb6169129a59281deb87d5215f6595
Lucas <lucasrian.dev@gmail.com>
2026-03-28 15:42:13 -0300

    treinando api

--- meu-primeiro-projeto-react/src/teste.jsx
+++ meu-primeiro-projeto-react/src/teste.jsx
@@ -1,5 +1,37 @@
 import { useState, useEffect } from "react";
 
-function Api() {}
+function Api() {
+  const [busca, setBusca] = useState("");
+  const [usuario, setUsuario] = useState(null);
 
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    if (busca === "") return;
+
+    fetch(`https://viacep.com.br/ws/${busca}/json/`)
+      .then((resposta) => resposta.json())
+      .then((dados) => {
+        console.log(dados);
+        setUsuario(dados);
+      })
+      .catch((erro) => {
+        console.log(erro, "deu erro!");
+      });
+  }, [busca]);
+
+  return (
+    <div>
+      <h1>Cep!</h1>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={() => setBusca(input)}>Buscar</button>
+
+      {usuario && <li>{usuario.bairro}</li>}
+    </div>
+  );
+}
 export default Api;
