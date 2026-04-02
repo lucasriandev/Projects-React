d41b2418eb63dca23d5eed44e65eedca4fa68e49
Lucas <lucasrian.dev@gmail.com>
2026-04-02 17:38:09 -0300

    treinando crud

--- meu-primeiro-projeto-react/src/Sobre.jsx
+++ meu-primeiro-projeto-react/src/Sobre.jsx
@@ -1,9 +1,49 @@
 import { useEffect, useState } from "react";
 
 function Crud() {
+  const [usuario, setUsuario] = useState(() => {
+    return JSON.parse(localStorage.getItem("usuarios")) || [];
+  });
+
+  const [novoUser, setNovoUser] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("usuarios", JSON.stringify(usuario));
+  }, [usuario]);
+
+  function add() {
+    if (novoUser !== "") {
+      setUsuario([...usuario, novoUser]);
+      setNovoUser("");
+    }
+  }
+
+  function remover(indexRemover) {
+    const novaLista = usuario.filter((t, index) => index !== indexRemover);
+    setUsuario(novaLista);
+    if (novaLista.length === 0) {
+      localStorage.removeItem("usuarios");
+    }
+  }
+
   return (
     <div>
-      <h1>Crud</h1>
+      <h1>Usuarios</h1>
+      <input
+        placeholder="digite"
+        type="text"
+        value={novoUser}
+        onChange={(e) => setNovoUser(e.target.value)}
+      />
+      <button onClick={add}>Cadastrar</button>
+      <ul>
+        {usuario.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button onClick={() => remover(index)}>🗑️</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
