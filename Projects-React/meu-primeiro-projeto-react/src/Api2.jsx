d622bde886af77fd5f677e2b734f122b3df807a3
Lucas <lucasrian.dev@gmail.com>
2026-04-19 18:02:50 -0300

    preparando nova api

--- Projects-React/meu-primeiro-projeto-react/src/Api2.jsx
+++ Projects-React/meu-primeiro-projeto-react/src/Api2.jsx
@@ -1,36 +1,7 @@
 import { useState, useEffect } from "react";
 
 function ApiPokemon() {
-  const [usuarios, setUsuarios] = useState([]);
-  const [carregando, setCarregando] = useState(false);
-
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((resposta) => resposta.json())
-      .then((dados) => {
-        console.log(dados);
-        setUsuarios(dados);
-        setCarregando(true);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-
-  if (carregando === false) {
-    return <h2>Buscando...</h2>;
-  }
-
-  return (
-    <div>
-      <h1>Usuarios!</h1>
-      <ul>
-        {usuarios.map((item, index) => {
-          return <li key={index}>{item.name}</li>;
-        })}
-      </ul>
-    </div>
-  );
+ 
 }
 
 export default ApiPokemon;
