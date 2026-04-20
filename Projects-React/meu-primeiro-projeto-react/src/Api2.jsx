b385883e8d36a65551145a162f772c71e87f90d0
Lucas <lucasrian.dev@gmail.com>
2026-04-20 17:13:28 -0300

    api

--- Projects-React/meu-primeiro-projeto-react/src/Api2.jsx
+++ Projects-React/meu-primeiro-projeto-react/src/Api2.jsx
@@ -1,7 +1,38 @@
 import { useState, useEffect } from "react";
 
 function ApiPokemon() {
- 
+  const [busca, setBusca] = useState("");
+  const [input, setInput] = useState("");
+  const [ceps, setCeps] = useState("");
+
+  useEffect(() => {
+    if (busca === "") return;
+
+    fetch(`https://viacep.com.br/ws/${busca}/json/`)
+      .then((resposta) => resposta.json())
+      .then((dados) => {
+        console.log(dados);
+        setCeps(dados);
+        setInput("");
+      })
+      .catch((erro) => {
+        console.log(erro, "errooooo");
+      });
+  }, [busca]);
+
+  return (
+    <div>
+      <h1>Pegando cep!</h1>
+      <input
+        type="number"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Digite seu cep"
+      />
+      <button onClick={() => setBusca(input)}>Buscar</button>
+      <ul>{ceps && <li>{ceps.bairro}</li>}</ul>
+    </div>
+  );
 }
 
 export default ApiPokemon;
