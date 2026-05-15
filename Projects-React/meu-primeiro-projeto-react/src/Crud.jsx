84905b1c9b9205979786da1bdc7e19497ac8b82f
Lucas <lucasrian.dev@gmail.com>
2026-05-15 17:51:16 -0300

    Treinando a logica dos id

--- Projects-React/meu-primeiro-projeto-react/src/Crud.jsx
+++ Projects-React/meu-primeiro-projeto-react/src/Crud.jsx
@@ -7,19 +7,46 @@ function Crud() {
 
   const [novoPersonagem, setNovoPersonagem] = useState("");
 
+  const [editandoId, setEditandoId] = useState(null);
+
   useEffect(() => {
     localStorage.setItem("Person", JSON.stringify(personagens));
   }, [personagens]);
 
   function salvar() {
-    if (novoPersonagem !== "") {
-      setPersonagens([...personagens, novoPersonagem]);
+    if (novoPersonagem === "") return;
+    if (editandoId === null) {
+      const personagem = {
+        id: Date.now(),
+        nome: novoPersonagem,
+      };
+      setPersonagens([...personagens, personagem]);
       setNovoPersonagem("");
+    } else {
+      const listaEditada = personagens.map((item) => {
+        if (item.id === editandoId) {
+          return {
+            ...item,
+            nome: novoPersonagem,
+          };
+        }
+
+        return item;
+      });
+
+      setPersonagens(listaEditada);
+      setEditandoId(null);
     }
+    setNovoPersonagem("");
+  }
+
+  function editar(personagem) {
+    setNovoPersonagem(personagem.nome);
+    setEditandoId(personagem.id);
   }
 
   function remover(indexRemover) {
-    const novaLista = personagens.filter((t, index) => index !== indexRemover);
+    const novaLista = personagens.filter((item) => item.id !== indexRemover);
     setPersonagens(novaLista);
     if (novaLista.length === 0) {
       localStorage.removeItem("Person");
@@ -37,10 +64,11 @@ function Crud() {
       />
       <button onClick={salvar}>Salvar</button>
       <ul>
-        {personagens.map((item, index) => (
-          <li key={index}>
-            {item}
-            <button onClick={() => remover(index)}>🗑️</button>
+        {personagens.map((item) => (
+          <li key={item.id}>
+            {item.nome}
+            <button onClick={() => editar(item)}>✏️</button>
+            <button onClick={() => remover(item.id)}>🗑️</button>
           </li>
         ))}
       </ul>
