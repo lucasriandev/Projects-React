62f9f926031ad243fc71c7cd06397c5bef789368
Lucas <lucasrian.dev@gmail.com>
2026-05-26 11:20:05 -0300

    terceira api

--- Projects-React/meu-primeiro-projeto-react/src/Api4.jsx
+++ Projects-React/meu-primeiro-projeto-react/src/Api4.jsx
@@ -3,9 +3,11 @@ import { useState, useEffect } from "react";
 function Paises() {
   const [paises, setPaises] = useState([]);
   const [input, setInput] = useState("");
+  const [busca, setBusca] = useState("");
 
   useEffect(() => {
-    fetch("https://restcountries.com/v3.1/all?fields=name,flags,population")
+    if (busca === "") return;
+    fetch(`https://restcountries.com/v3.1/name/${busca}`)
       .then((res) => res.json())
       .then((dados) => {
         console.log(dados);
@@ -14,7 +16,30 @@ function Paises() {
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  }, [busca]);
+
+  return (
+    <div>
+      <h1>Paises</h1>
+      <p>Busque o seu!</p>
+      <input
+        type="text"
+        placeholder="Digite"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={() => setBusca(input)}>Buscar</button>
+      <ul>
+        {paises.map((item) => (
+          <li>
+            {item.name.common}
+            <br></br>
+            <img src={item.flags.png} alt="" />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Paises;
