05308b0c76267ff08c5ef2634b31aaf047668a94
Lucas <lucasrian.dev@gmail.com>
2026-05-31 22:10:52 -0300

    removendo para amanha

--- Projects-React/meu-primeiro-projeto-react/src/Api4.jsx
+++ Projects-React/meu-primeiro-projeto-react/src/Api4.jsx
@@ -1,45 +1,7 @@
 import { useState, useEffect } from "react";
 
 function Paises() {
-  const [paises, setPaises] = useState([]);
-  const [input, setInput] = useState("");
-  const [busca, setBusca] = useState("");
-
-  useEffect(() => {
-    if (busca === "") return;
-    fetch(`https://restcountries.com/v3.1/name/${busca}`)
-      .then((res) => res.json())
-      .then((dados) => {
-        console.log(dados);
-        setPaises(dados);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, [busca]);
-
-  return (
-    <div>
-      <h1>Paises</h1>
-      <p>Busque o seu!</p>
-      <input
-        type="text"
-        placeholder="Digite"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      />
-      <button onClick={() => setBusca(input)}>Buscar</button>
-      <ul>
-        {paises.map((item) => (
-          <li>
-            {item.name.common}
-            <br></br>
-            <img src={item.flags.png} alt="" />
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  fetch(`https://restcountries.com/v3.1/name/${busca}`);
 }
 
 export default Paises;
