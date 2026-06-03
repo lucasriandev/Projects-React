36a20e21138d077339e28734470207173775c0e6
Lucas <lucasrian.dev@gmail.com>
2026-06-03 16:57:47 -0300

    api4 refeita

--- Projects-React/meu-primeiro-projeto-react/src/Api4.jsx
+++ Projects-React/meu-primeiro-projeto-react/src/Api4.jsx
@@ -1,7 +1,42 @@
 import { useState, useEffect } from "react";
 
 function Paises() {
-  fetch(`https://restcountries.com/v3.1/name/${busca}`);
+  const [paises, setPaises] = useState([]);
+  const [input, setInput] = useState("");
+  const [busca, setBusca] = useState("");
+
+  useEffect(() => {
+    if (busca === "") return;
+    fetch(`https://restcountries.com/v3.1/name/${busca}`)
+      .then((resp) => resp.json())
+      .then((dados) => {
+        console.log(dados);
+        setPaises(dados);
+      })
+      .catch((error) => console.log(error));
+  }, [busca]);
+
+  return (
+    <div>
+      <h1>Paises!</h1>
+      <input
+        type="text"
+        placeholder="Digite um pais"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={() => setBusca(input)}>Salvar</button>
+      <ul>
+        {paises.map((item) => (
+          <li>
+            {item.name.common}
+            <br></br>
+            <img src={item.flags.png} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Paises;
