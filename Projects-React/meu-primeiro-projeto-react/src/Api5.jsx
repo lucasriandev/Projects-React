b2bee299c79ae58a743e6a682bbca3ba780a0f73
Lucas <lucasrian.dev@gmail.com>
2026-06-19 12:14:08 -0300

    apagando para refazer

--- Projects-React/meu-primeiro-projeto-react/src/Api5.jsx
+++ Projects-React/meu-primeiro-projeto-react/src/Api5.jsx
@@ -1,42 +1,7 @@
 import { useState, useEffect } from "react";
 
 function Receitas() {
-  const [receitas, setReceitas] = useState([]);
-  const [busca, setBusca] = useState("");
-  const [input, setInput] = useState("");
-
-  useEffect(() => {
-    if (busca === "") return;
-
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${busca}`)
-      .then((resp) => resp.json())
-      .then((dados) => {
-        console.log(dados);
-        setReceitas(dados.meals || []);
-        setInput("");
-      })
-      .catch((erro) => {
-        console.log(erro, "404");
-      });
-  }, [busca]);
-
-  return (
-    <div>
-      <h2>Receitas</h2>
-      <input
-        type="text"
-        placeholder="Digite!"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      />
-      <button onClick={() => setBusca(input)}>Buscar</button>
-      <ul>
-        {receitas.map((item) => (
-          <li>{item.strMeal}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${busca}`);
 }
 
 export default Receitas;
--- Projects-React/meu-primeiro-projeto-react/src/Crud.jsx
+++ Projects-React/meu-primeiro-projeto-react/src/Crud.jsx
@@ -1,78 +1,7 @@
 import { useState, useEffect } from "react";
 
 function Crud() {
-  const [lutadores, setLutadores] = useState(() => {
-    return JSON.parse(localStorage.getItem("lutadores")) || [];
-  });
-
-  const [input, setInput] = useState("");
-  const [id, setId] = useState(null);
-
-  useEffect(() => {
-    localStorage.setItem("lutadores", JSON.stringify(lutadores));
-  }, [lutadores]);
-
-  function salvar() {
-    if (input === "") return;
-    if (id === null) {
-      const novaLista = {
-        id: Date.now(),
-        texto: input,
-      };
-      setLutadores([...lutadores, novaLista]);
-      setInput("");
-    } else {
-      const listaNova = lutadores.map((item) => {
-        if (item.id === id) {
-          return {
-            ...item,
-            texto: input,
-          };
-        }
-        return item;
-      });
-      setLutadores(listaNova);
-      setId(null);
-      setInput("");
-    }
-  }
-
-  function editar(ed) {
-    setInput(ed.texto);
-    setId(ed.id);
-  }
-
-  function remover(indexRemover) {
-    const novaLista = lutadores.filter((item) => item.id !== indexRemover);
-    setLutadores(novaLista);
-    if (novaLista.length === 0) {
-      localStorage.removeItem("lutadores");
-    }
-  }
-
-  return (
-    <div>
-      <h1>UFC CASA BRANCA!</h1>
-      <input
-        type="text"
-        placeholder="Digite!"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      />
-      <button onClick={() => salvar()}>Salvar</button>
-      <ul>
-        {lutadores.map((item) => {
-          return (
-            <li key={item.id}>
-              {item.texto}
-              <button onClick={() => editar(item)}>🖊️</button>
-              <button onClick={() => remover(item.id)}>❌</button>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
+  
 }
 
 export default Crud;
