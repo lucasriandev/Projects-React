9668ae475b2ad9162076acd8e4d8605c5acfe67c
Lucas <lucasrian.dev@gmail.com>
2026-06-22 17:51:07 -0300

    refazendo receitas

--- Projects-React/meu-primeiro-projeto-react/src/Api5.jsx
+++ Projects-React/meu-primeiro-projeto-react/src/Api5.jsx
@@ -1,6 +1,39 @@
 import { useState, useEffect } from "react";
 //  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${busca}`);
 
-function Receitas() {}
+function Receitas() {
+  const [receitas, setReceitas] = useState([]);
+  const [busca, setBusca] = useState("");
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    if (busca === "") return;
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${busca}`)
+      .then((res) => res.json())
+      .then((dados) => {
+        console.log(dados);
+        setReceitas(dados.meals);
+      })
+      .catch((erro) => {
+        console.log(erro);
+      });
+  }, [busca]);
+
+  return (
+    <div>
+      <h1>Receitas</h1>
+      <input
+        type="text"
+        placeholder="Digite"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={() => setBusca(input)}>Buscar</button>
+      {receitas.map((item, index) => {
+        return <li key={index}>{item.strMeal}</li>;
+      })}
+    </div>
+  );
+}
 
 export default Receitas;
