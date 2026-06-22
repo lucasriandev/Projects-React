fd774652ba9991bb39973a0fd0095fc164440e3b
Lucas <lucasrian.dev@gmail.com>
2026-06-22 17:04:44 -0300

    api em filtros

--- Projects-React/meu-primeiro-projeto-react/src/Api3.jsx
+++ Projects-React/meu-primeiro-projeto-react/src/Api3.jsx
@@ -2,9 +2,48 @@ import { useState, useEffect } from "react";
 //fetch(`https://jsonplaceholder.typicode.com/users`)
 
 function ApiNome() {
- 
-    fetch(`https://pokeapi.co/api/v2/pokemon/${busca}`)
-     
+  const [pokemons, setPokemons] = useState([]);
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon")
+      .then((res) => res.json())
+      .then((dados) => {
+        console.log(dados);
+        setPokemons(dados.results);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const filtros =
+    input === ""
+      ? pokemons
+      : pokemons.filter((item) =>
+          item.name.toLowerCase().includes(input.toLocaleLowerCase()),
+        );
+
+  return (
+    <div>
+      <h1>Pokemons</h1>
+      <input
+        type="text"
+        placeholder="Digite"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      {filtros.length === 0 ? (
+        <h2>Nenhum pokemon!</h2>
+      ) : (
+        <ul>
+          {filtros.map((item) => (
+            <li>{item.name}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
 
 export default ApiNome;
--- Projects-React/meu-primeiro-projeto-react/src/Api5.jsx
+++ Projects-React/meu-primeiro-projeto-react/src/Api5.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
+//  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${busca}`);
 
-function Receitas() {
-  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${busca}`);
-}
+function Receitas() {}
 
 export default Receitas;
